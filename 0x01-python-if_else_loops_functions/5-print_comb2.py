44e3adfe0a2af26cbca3611f1dd0df582bc4ddf8
Mardie328 <tolayinka328@gmail.com>
2023-08-10 20:49:20 -0700

    add new file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(100):
-    if (number >= 0 and number <= 99):
-        print("{}{}, ".format((number // 10), number % 10))
+    if (number >= 0 and number <= 98):
+        print("{}{}, ".format((number // 10), number % 10), end="")
     else:
-        print("{}{}, ".format(int(number / 10), number % 10), end="")
+        print("{}{}".format((number // 10), number % 10))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
- #!/usr/bin/python3
-def uppercase(string):
-    for char in string:
-        if ord(char) >= 97 and ord(char) <= 122:
-            upper_case = ord(char) - 32
-            print("{}".format(chr(upper_case)), end='')
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            num = 32
         else:
-            print("{}".format(char), end='')
-    return \n
-        
+            num = 0
+            print("{:c}".format(ord(str[i]) - num), end='')
+    print()
