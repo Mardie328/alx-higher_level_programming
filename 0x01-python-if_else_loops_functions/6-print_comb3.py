c18759cf6461e85c16502302f9ddf0db74c44edf
Mardie328 <tolayinka328@gmail.com>
2023-08-12 02:38:03 -0700

    add new file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for number in range(100):
     if (((number // 10) != number % 10) and ((number // 10) < number % 10)):
-        print("{}{},".format((number // 10), number % 10), end=" ")
+        if number == 89:  # Last number in the loop
+            print("{}{}".format((number // 10), number % 10), end=" ")
+        else:
+            print("{}{}".format((number // 10), number % 10), end=", ")
 print()
