31d0400a3f9df8379bb2c2dac82e9600896a5c57
Mardie328 <tolayinka328@gmail.com>
2023-08-12 11:26:18 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-# Get the number of arguments
-argc = len(sys.argv) - 1
+    # Get the number of arguments
+    argc = len(sys.argv) - 1
 
-# Get the list of arguments
-argv = sys.argv[1:]
+    # Get the list of arguments
+    argv = sys.argv[1:]
 
-# Print the header
-print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
+    # Print the header
+    print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
 
-# Print each argument and its position
-for i in range(argc):
-    position = i + 1
-    argument = argv[i]
-    print("{}: {}".format(position, argument))
+    # Print each argument and its position
+    for i in range(argc):
+        position = i + 1
+        argument = argv[i]
+        print("{}: {}".format(position, argument))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argc = len(sys.argv)
-total_sum = 0
+    argc = len(sys.argv)
+    total_sum = 0
 
-# Calculate the sum of all arguments
-for i in range(1, argc):
-    total_sum += int(sys.argv[i])
+    # Calculate the sum of all arguments
+    for i in range(1, argc):
+        total_sum += int(sys.argv[i])
 
-# Print the result
-print(total_sum)
+    print(total_sum)
