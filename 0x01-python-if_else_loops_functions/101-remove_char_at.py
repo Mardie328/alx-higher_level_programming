ba226609cc3cf0bf0f877cb98065ca71f1aad6d4
Mardie328 <tolayinka328@gmail.com>
2023-08-12 04:30:49 -0700

    add new file

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+def remove_char_at(str, n):
+    if n >= 0 and n < len(str):
+        new_str = ""
+        for i in range(len(str)):
+            if i != n:
+                new_str += str[i]
+        return new_str
+    else:
+        return str
