4c5be8185e8514329395effd9b4b30c98da98a59
Mardie328 <tolayinka328@gmail.com>
2023-08-16 15:29:52 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    import sys
+    i = 1
+    args = len(argv) - 1
 
-    # Get the number of arguments
-    argc = len(sys.argv) - 1
-
-    # Get the list of arguments
-    argv = sys.argv[1:]
-
-    # Print the header
-    print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
-
-    # Print each argument and its position
-    for i in range(argc):
-        position = i + 1
-        argument = argv[i]
-        print("{}: {}".format(position, argument))
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")
+    while (i <= args):
+        print(f"{i}: {argv[i]}")
+        i += 1
