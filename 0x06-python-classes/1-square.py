1dc4dedb7236b213d11336b143e5767c5802434e
Mardie328 <tolayinka328@gmail.com>
2023-08-30 17:21:50 -0700

    add new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by size"""
+
+
+class Square:
+    """a class Square that defines a square by size"""
+
+    def __init__(self, square_size):
+        """Square constructor"""
+        self.__size = square_size
