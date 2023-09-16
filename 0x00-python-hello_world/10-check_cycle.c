6aec6c922900c8b6bade21baf1aa5740b7ccbde8
Mardie328 <tolayinka328@gmail.com>
2023-09-16 10:07:50 -0700

    add new file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a listint_t list has a cycle in it
+ * @list: pointer to the list to be checked
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	if (list == NULL)
+	{
+		printf("Linked list is empty");
+		return (0);
+	}
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
