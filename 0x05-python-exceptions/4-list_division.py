f70ddd7040d1ad11ff013e844d3902cffcf5dc37
Mardie328 <tolayinka328@gmail.com>
2023-09-24 07:52:00 +0100

    add new file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    result = 0
     for idx in range(list_length):
-        result = 0
         try:
             result = my_list_1[idx] / my_list_2[idx]
         except (ZeroDivisionError):
@@ -13,4 +13,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print('out of range')
         finally:
             new_list.append(result)
-    return (new_list)`
+    return (new_list)
