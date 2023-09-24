30fd6ecbb03932e1865c9246e4223667caf32e60
Mardie328 <tolayinka328@gmail.com>
2023-09-24 21:57:02 +0100

    fix: pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8) and 
-    returns the number of characters written
+"""a function that writes a string to a text file (UTF8) and
+returns the number of characters written
 """
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file and returns the number of characters written"""
+    """Writes a string to a text file and
+    returns the number of characters written
+    """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         return len(text)
